019f20b4fc4202940ef237f8b61e5a47df175b47
Yuri Nunes <yuriasn@hotmail.com>
2026-02-05 19:52:50 +0000

    Added security and double rotate
    Security against write() error was implemented by calling end_all() in case -1 was returned.
    Double rotate was implemented to work with rotation or reverse rotation depending on the flag given

--- commands.c
+++ commands.c
@@ -2,27 +2,27 @@
 
 /** @brief Swaps the values of the 2 first nodes of the list
  * and prints the command or not accordingly to the flag.
- * @param stack
- * Pointer of the linked list.
- * @param flag
- * Tells the name of the list, or nothing if isn't supposed to write.
- * @return
- * Amount of chars writted to stdout. 0 in case nothing
+ * @param stack Pointer of the linked list.
+ * @param flag Tells the name of the list, or nothing if isn't supposed to write.
+ * @param error If existing, the other stack is sent to handle errors in write.
+ * If not, send NULL.
+ * @return Amount of chars writted to stdout. 0 in case nothing
  * needs to be written or exist. -1 in case of error. */
-int	swap(t_stacks *stack, char flag)
+void	swap(t_stacks *stack, char flag, t_stacks *error)
 {
 	int	tmp;
 
 	if (!stack->next)
-		return (0);
+		return ;
 	tmp = stack->nbr;
 	stack->nbr = stack->next->nbr;
 	stack->next->nbr = tmp;
 	if (flag == 'a')
-		return (ft_putendl_fd("sa", 1));
+		if (ft_putendl_fd("sa", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 	if (flag == 'b')
-		return (ft_putendl_fd("sb", 1));
-	return (0);
+		if (ft_putendl_fd("sb", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 }
 
 /** @brief Pushes the first node of the list from to to.
@@ -31,12 +31,12 @@ int	swap(t_stacks *stack, char flag)
  * @param flag	Id of the stack that the node is coming from.
  * @return Amout of chars written. 0 if list doesn't exist.
  * -1 in case of error. */
-int	push(t_stacks **stack_from, t_stacks **stack_to, char flag)
+void	push(t_stacks **stack_from, t_stacks **stack_to, char flag)
 {
 	t_stacks	*tmp;
 
 	if (!*stack_from)
-		return (0);
+		return ;
 	tmp = *stack_from;
 	*stack_from = (*stack_from)->next;
 	tmp->next = NULL;
@@ -44,39 +44,46 @@ int	push(t_stacks **stack_from, t_stacks **stack_to, char flag)
 		tmp->next = *stack_to;
 	*stack_to = tmp;
 	if (flag == 'a')
-		return (ft_putendl_fd("pb", 1));
-	return (ft_putendl_fd("pa", 1));
+		if (ft_putendl_fd("pb", 1) == -1)
+			end_all(*stack_from, *stack_to, WRITE_ERROR);
+	if (ft_putendl_fd("pa", 1) == -1)
+		end_all(stack_from, stack_to, WRITE_ERROR);
 }
 
 /** @brief Rotates the stack passing first element to be the last.
  * @param stack	Pointer to the first node of list to rotate.
  * @param flag	Flag with the name of the stack to print the move.
  * Or NULL if nothing has to be printed (rr case).
+ * @param error If existing, the other stack is sent to handle errors in write.
+ * If not, send NULL.
  * @return	Amount of chars writted to stdout. 0 in case nothing
  * needs to be written or exist. -1 in case of error. */
-int	rotate(t_stacks **stack, char flag)
+void	rotate(t_stacks **stack, char flag, t_stacks *error)
 {
 	t_stacks	*tmp;
 
 	if (!(*stack)->next)
-		return (0);
+		return ;
 	tmp = *stack;
 	*stack = (*stack)->next;
 	tmp->next = NULL;
 	ft_lstlast(*stack)->next = tmp;
 	if (flag == 'a')
-		return (ft_putendl_fd("ra", 1));
+		if (ft_putendl_fd("ra", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 	if (flag == 'b')
-		return (ft_putendl_fd("rb", 1));
-	return (0);
+		if (ft_putendl_fd("rb", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 }
 /** @brief Reverse rotates the stack passing last element to be the first.
  * @param stack	Pointer to the first node of list to rotate.
  * @param flag	Flag with the name of the stack to print the move.
  * Or NULL if nothing has to be printed (rr case).
+ * @param error If existing, the other stack is sent to handle errors in write.
+ * If not, send NULL.
  * @return	Amount of chars writted to stdout. 0 in case nothing
  * needs to be written or exist. -1 in case of error. */
-int	rev_rotate(t_stacks **stack, char flag)
+void	rev_rotate(t_stacks **stack, char flag, t_stacks *error)
 {
 	t_stacks	*tmp;
 
@@ -87,8 +94,32 @@ int	rev_rotate(t_stacks **stack, char flag)
 	ft_lstlast(*stack)->next = tmp;
 	tmp->next = NULL;
 	if (flag == 'a')
-		return (ft_putendl_fd("rra", 1));
+		if (ft_putendl_fd("rra", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 	if (flag == 'b')
-		return (ft_putendl_fd("rrb", 1));
+		if (ft_putendl_fd("rrb", 1) == -1)
+			end_all(stack, error, WRITE_ERROR);
 	return (0);
+}
+
+/** @brief Do the double rotation to the side decided by the flag.
+ * @param a Pointer to linked list a.
+ * @param b Pointer to linked list b.
+ * @param way TRUE if is normal rotation, FALSE if is reverse. */
+void	double_rotation(t_stacks **a, t_stacks **b, t_bool way)
+{
+	if (way)
+	{
+		rotate(a, (char)0, b);
+		rotate(b, (char)0, a);
+		if (ft_putendl_fd("rr", 2) == -1)
+			end_all(*a, *b, WRITE_ERROR);
+	}
+	else
+	{
+		rev_rotate(a, (char)0, b);
+		rev_rotate(b, (char)0, a);
+		if (ft_putendl_fd("rrr", 2) == -1)
+			end_all(*a, *b, WRITE_ERROR);
+	}
 }
