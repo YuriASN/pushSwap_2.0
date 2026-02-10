c024d93c2fb9d68f36452d440b7c01d412baaf68
Yuri Nunes <yuriasn@hotmail.com>
2026-02-10 16:04:45 +0000

    Moved get functions to getter.c and added rotate_amount() here.
    rotate_amount() will rotate a stack x amount of times, using rotate() for positive numbers and rev_rotate() for negative.

--- helpers.c
+++ helpers.c
@@ -1,79 +1,19 @@
 #include "push_swap.h"
 
-/** @brief Search for the 2 lowest numbers on the stack.
- * @param stack Stack to search on.
- * @param l1 Pointer to attribute the lowest number.
- * @param l2 Pointer to attribute the 2nd lowest.
- * NULL if you only need the lowest. */
-void	get_lowest(t_stacks *stack, int *l1, int *l2)
+/** @brief Rotates the stack the amount of times of roll.
+ * Rotating reverse if roll is a negative number.
+ * @param stack Pointer to linked list to rotate.
+ * @param roll Amount of rotations need to be done.
+ * @param id Id of the stack being rotated.
+ * @param error The other linked list to work with it in case of error. */
+void	rotate_amount(t_stacks **stack, int roll, char id, t_stacks *error)
 {
-	t_stacks	*tmp;
-
-	tmp = stack;
-	*l1 = tmp->nbr;
-	if (l2)
-		*l2 = INT_MAX;
-	while (tmp->next)
-	{
-		tmp = tmp->next;
-		if (*l1 > tmp->nbr)
-		{
-			if (*l2)
-				*l2 = *l1;
-			*l1 = tmp->nbr;
-		}
-		else if (l2 && (*l2 > tmp->nbr || *l2 == *l1))
-			*l2 = tmp->nbr;
-	}
-}
-
-/** @brief Search for the 2 highest numbers on the stack.
- * @param stack Stack to search on.
- * @param h1 Pointer to attribute the highest number.
- * @param h2 Pointer to attribute the 2nd highest. NULL if you only want one */
-void	get_highest(t_stacks *stack, int *h1, int *h2)
-{
-	t_stacks	*tmp;
-
-	tmp = stack;
-	*h1 = tmp->nbr;
-	if (h2)
-		*h2 = INT_MIN;
-	while (tmp->next)
-	{
-		tmp = tmp->next;
-		if (*h1 < tmp->nbr)
-		{
-			if (h2)
-				*h2 = *h1;
-			*h1 = tmp->nbr;
-		}
-		else if (h2 && (*h2 < tmp->nbr || *h2 == *h1))
-			*h2 = tmp->nbr;
-	}
-}
-
-/** @brief Return the index of the node that has the given number.
- * @param stack Linked list to search on.
- * @param nbr	Number you're looking for.
- * @return Index of the node that number was found. */
-int	get_index(t_stacks *stack, int nbr)
-{
-	int			i;
-	t_stacks	*tmp;
-
-	if (!stack)
-		return (0);
-	i = 0;
-	tmp = stack;
-	while (tmp)
-	{
-		if (tmp->nbr == nbr)
-			return (i);
-		i++;
-		tmp = tmp->next;
-	}
-	return (i);
+		if (roll > 0)
+			while (roll--)
+				rotate(stack, id, error);
+		else
+			while (roll++)
+				rev_rotate(stack, id, error);
 }
 
 /** @brief Takes a Stack and a number that it has, choose wich rotation will use
